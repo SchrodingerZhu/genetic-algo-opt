8cbf342941247950024c70defd2fcf6cf74a26a8
Outvi V <git@outv.im>
2020-11-21 20:54:57 +0800

    feat: fitness

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,32 @@
-use rand::Rng;
 use crate::*;
+use rand::Rng;
 #[derive(Clone, Debug)]
 pub struct Instance {
-    pub(crate) gene: Vec<usize>
+    pub(crate) gene: Vec<usize>,
+}
+
+fn generate_meta(
+    gene: &[usize],
+    sizes: &[usize],
+    pgsize: usize,
+) -> (
+    hashbrown::HashMap<usize, usize>,
+    hashbrown::HashMap<usize, usize>,
+) {
+    let mut ret1 = hashbrown::HashMap::<usize, usize>::new();
+    let mut ret2 = hashbrown::HashMap::<usize, usize>::new();
+    let mut page_id = 0;
+    let mut curr_byte = 0;
+    for i in gene {
+        ret1.insert(*i, page_id);
+        ret2.insert(*i, curr_byte);
+        curr_byte += sizes[*i];
+        if curr_byte > pgsize {
+            page_id += 1;
+            curr_byte -= pgsize;
+        }
+    }
+    (ret1, ret2)
 }
 
 impl Instance {
@@ -23,9 +47,9 @@ impl Instance {
                 std::mem::swap(&mut x2, &mut y2);
             }
 
-            if x1 == y1 || x1 == x2
-                || x1 == y2 || y1 == x2
-                || y1 == y2 || x2 == y2 { continue; }
+            if x1 == y1 || x1 == x2 || x1 == y2 || y1 == x2 || y1 == y2 || x2 == y2 {
+                continue;
+            }
 
             // now we exchange x1 <-> x2, y1 <-> y2
             self.gene.iter_mut().for_each(|x| {
@@ -67,19 +91,29 @@ impl Instance {
             }
             i += 1;
         }
-        Instance {
-            gene
-        }
+        Instance { gene }
     }
 
     pub fn fitness(&self, graph: &crate::graph::Graph) -> f64 {
         //final fitness: scale factor / penalty
-        unimplemented!()
+        let mut penalty = 0f64;
+
+        let (id_to_page_map, id_to_relpos_map) = generate_meta(&self.gene, &graph.sizes, PAGE_SIZE);
+
+        for ((f, t), freq) in &graph.edges {
+            if id_to_page_map.get(f) != id_to_page_map.get(t) {
+                penalty += *freq as f64 * PAGE_FAULT_PENALTY;
+            }
+            if id_to_relpos_map.get(f) != id_to_relpos_map.get(t) {
+                penalty += *freq as f64 * CACHE_MISS_PENALTY;
+            }
+        }
+        SCALE_FACTOR / penalty
     }
 
-    pub fn mate(a : &Self, b: &Self) -> Self {
+    pub fn mate(a: &Self, b: &Self) -> Self {
         let mut rng = rand::thread_rng();
-        let rand : f64 = rng.gen();
+        let rand: f64 = rng.gen();
         let mut target = if rand <= 0.45 {
             a.clone()
         } else if rand <= 0.90 {
